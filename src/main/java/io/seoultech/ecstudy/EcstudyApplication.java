7a3f84eecdddefa1d7e1967112564562d809cf56
jdyj <jdyj@naver.com>
2022-11-09 13:52:29 +0900

    init

---
+++ src/main/java/io/seoultech/ecstudy/EcstudyApplication.java
@@ -0,0 +1,13 @@
+package io.seoultech.ecstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/seoultech/ecstudy/EcstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.seoultech.ecstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
